b9ba3ea1c93e5e4629eed0e81bdbf8dfc44ee168
nadezhdalex <63705797+nadezhdalex@users.noreply.github.com>
2023-11-12 16:25:05 +0300

    Update User table in db_orm.py

--- src/database/db_orm.py
+++ src/database/db_orm.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Integer, String, Column
+from sqlalchemy import create_engine, Integer, String, Column, ARRAY, DateTime, Boolean
 
 from sqlalchemy.orm import declarative_base
 from envs.db_secrets import db_url
@@ -7,13 +7,12 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
-    user_id = Column(Integer(), primary_key=True)
+    user_id = Column(Integer(), autoincrement=False, primary_key=True)
     user_name = Column(String(100), nullable=False)
+    create_date = Column(DateTime(), nullable=False)
+    subscribes = Column(ARRAY(Boolean), nullable=False)
 
 
 if __name__ == '__main__':
     engine = create_engine(db_url)
     Base.metadata.create_all(engine)
-
-
-
