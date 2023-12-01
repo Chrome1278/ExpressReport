98e110537a8fd6c17f9989d812fbbfa0e2f1ca4c
nadezhdalex <63705797+nadezhdalex@users.noreply.github.com>
2023-12-01 16:04:16 +0300

    Add table for assets

--- src/database/db_orm.py
+++ src/database/db_orm.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, Integer, String, Column, ARRAY, DateTime, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy_utils import create_database, drop_database, database_exists
+from sqlalchemy_utils import create_database, drop_database
 from sqlalchemy.orm import declarative_base
 from envs.db_secrets import db_url_old
 from sqlalchemy.sql import func
@@ -24,6 +24,21 @@ class UserLogInfo(Base):
     subscribes_before = Column(ARRAY(Boolean), nullable=False)
     subscribes_after = Column(ARRAY(Boolean), nullable=False)
     change_date = Column(DateTime(), nullable=False, server_default=func.now())
+
+
+class AssetUpdates(Base):
+    __tablename__ = 'asset_update'
+    update_id = Column(Integer(), primary_key=True)
+    asset_name = Column(String(32), nullable=False)
+    n_buy = Column(Integer(), nullable=False)
+    n_sell = Column(Integer(), nullable=False)
+    n_neutral = Column(Integer(), nullable=False)
+    recommendation = Column(String(32), nullable=False)
+    open_price = Column(Integer(), nullable=False)
+    close_price = Column(Integer(), nullable=False)
+    low_price = Column(Integer(), nullable=False)
+    high_price = Column(Integer(), nullable=False)
+    update_date = Column(DateTime(), nullable=False, server_default=func.now())
 
 
 if __name__ == '__main__':
